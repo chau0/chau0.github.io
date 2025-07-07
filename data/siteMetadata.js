2dcb65bc4405d4a685fb5351cceb2c7d50816387
Chau <haichaule1990@gmail.com>
2025-07-07 23:02:20 +0800

    Add initial blog content and update site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,9 +1,10 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Le Chau - Indie Developer',
+  author: 'Le Hai Chau',
+  headerTitle: 'Le Hai Chau',
+  description:
+    'Thoughts, projects, and reflections from a developer exploring AI, automation, and mindful creation.',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
@@ -15,13 +16,13 @@ const siteMetadata = {
   github: 'https://github.com',
   x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
+  // facebook: 'https://facebook.com',
+  // youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
+  // threads: 'https://www.threads.net',
+  // instagram: 'https://www.instagram.com',
   medium: 'https://medium.com',
-  bluesky: 'https://bsky.app/',
+  // bluesky: 'https://bsky.app/',
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
